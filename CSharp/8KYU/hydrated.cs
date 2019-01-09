fddf3f6a6e601bed81744b66ad8f62b9dd07eccb
aldwa1979 <45319240+aldwa1979@users.noreply.github.com>
2019-01-09 16:15:35 +0100

    Keep Hydrated!

---
+++ CSharp/8KYU/hydrated.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Kata
+{
+  public static int Litres(double time)
+  {
+    double water = 0.5;
+    double drink = (water * time);
+            return (int)drink;
+  }
+}
