22ea56e4226b87d795d353554b7d44dcc74afb16
aldwa1979 <45319240+aldwa1979@users.noreply.github.com>
2019-01-10 08:44:22 +0100

    Remove First and Last Character

---
+++ CSharp/8KYU/remove.cs
@@ -0,0 +1,9 @@
+using System;
+
+        public class Kata
+        {
+            public static string Remove_char(string s)
+            {
+                return s.Remove(0,1).Remove(s.Length-2,1);
+            }
+        }
